3e2f866b5e98c1395f4aa80578a6a3beebc86db4
John Morrison <jslmorrison@gmail.com>
2023-04-22 13:52:58 +0100

    Customize options for treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "php",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    auto_install = true,
+  }
+}
